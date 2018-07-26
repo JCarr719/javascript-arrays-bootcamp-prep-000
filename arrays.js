6476f4cf5f81ddbce03922ea31e2f39aee762dc0
josh.ray.carr@gmail.com <josh.ray.carr@gmail.com>
2018-07-26 23:50:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ function addElementToBeginningOfArray(array,element){
   return [element,...(array)]
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
-  return (arrays).unshift(element)
+  array.unshift(element)
+  return array
 }
